e378214c0cc52966fbe8a66f9f0401cb703f39c4
alexeykachan03 <71043125+alexeykachan03@users.noreply.github.com>
2021-05-09 21:59:58 +0300

    Update NumberConverter.m

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -4,17 +4,21 @@
 
 - (NSArray *)numberConverter:(NSNumber *)number {
 	NSNumber *number2;
-	if([number intValue] < 0) {
-		number2 = [NSNumber numberWithLongLong:-[number longLongValue]];
-	} else {
+		if([number intValue] < 0) {
+			number2 = [NSNumber numberWithLongLong:-[number longLongValue]];
+			} 
+	else {
 		number2 = [NSNumber numberWithLongLong:[number longLongValue]];
 	}
+	
 	NSString *myString = [number2 stringValue];
+	
 	NSMutableArray *arr = [[NSMutableArray alloc] initWithCapacity:[myString length] ];
+	
 	for(long i = [myString length] - 1; i >= 0; i--) {
 		[arr addObject:[myString substringWithRange:NSMakeRange(i, 1)]];
 	}
 	return arr;
-}
+		}
 
 @end
